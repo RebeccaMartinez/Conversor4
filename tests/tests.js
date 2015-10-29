476eb53304e1807928b4cfb914e527c7995a1ca6
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-10-29 18:15:27 +0000

    añadidos los tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,100 +2,101 @@ var expect = chai.expect;
 
 describe("Tests del Conversor", function() {
 
-    it("Debería ser: 32e4F", function() {
+    it("-3.4F === 3-19.67 Celsius, 253.48Kelvin", function() {
       var temp = new Temperatura();
 
-      temp.set_valor(32e4);
-      temp.set_tipo("F");
+      temp.setValor(-3.4);
+      temp.setTipo("F");
 
       var espia = sinon.spy();
-      espia(temp.get_valor());
+      espia(temp.getValor());
 
       expect(espia.called).to.be.true;
       expect(espia.calledOnce).to.be.true;
-      expect(espia.firstCall.calledWith(32e4)).to.be.true;
+      expect(espia.firstCall.calledWith(-3.4)).to.be.true;
       expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
 
-      expect(temp.get_valor()).to.equal(32e4);
-      expect(temp.get_tipo()).to.equal("F");
+      var aux = temp.calculate();
+
+      expect(aux).to.equal("-19.67 Celsius, 253.48Kelvin");
     });
+
+
+    it("41.4e3c === 3-19.67 Celsius, 253.48Kelvin", function() {
+      var temp = new Temperatura();
+
+      temp.setValor(41.4e3);
+      temp.setTipo("c");
+
+      var espia = sinon.spy();
+      espia(temp.getValor());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith(41.4e3)).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
+
+      var aux = temp.calculate();
+
+      expect(aux).to.equal("74552.00 Fahrenheit, 41673.15Kelvin");
+    });
+
+
+    it("-6.3k === 3-19.67 Celsius, 253.48Kelvin", function() {
+      var temp = new Temperatura();
+
+      temp.setValor(-6.3);
+      temp.setTipo("k");
+
+      var espia = sinon.spy();
+      espia(temp.getValor());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith(-6.3)).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
+
+      var aux = temp.calculate();
+
+      expect(aux).to.equal("-279.45Celsius, -471.01Fahrenheit");
+    });
+
+    it("Resultado: 22e3K", function() {
+      var temp = new Temperatura();
+
+      temp.setValor(22e3);
+      temp.setTipo("K");
+
+      var espia = sinon.spy();
+      espia(temp.getValor());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith(22e3)).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
+
+      expect(temp.getValor()).to.equal(22e3);
+      expect(temp.getTipo()).to.equal("K");
+    });
+
+
+    it("Resultado: -13.6E-4C", function() {
+      var temp = new Temperatura();
+      temp.setValor(-13.6E-4);
+      temp.setTipo("C");
+
+      var espia = sinon.spy();
+      espia(temp.getTipo());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith("C")).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.string)).to.be.true;
+
+      expect(temp.getValor()).to.equal(-13.6E-4);
+      expect(temp.getTipo()).to.equal("C");
+    });
+
+
 });
 
-// var expect = chai.expect;
-
-// describe("Tests BDD", function() {
-
-//    var aux = document.getElementById("converted");
-
-//   it("-12.3C === 9.86 Farenheit", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(-12.3);
-//     temp.setTipo("C");
-//     var res = temp.caf();
-//     expect(res).to.equal(9.86);
-//   });
-
-//   it(" 32c === 305.15Kelvin", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(32);
-//     temp.setTipo("c");
-//     var res = temp.cak();
-//     expect(res).to.equal(305.15);
-//   });
-
-//   it("25e4k === 249726.85 Celsius", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(25e4);
-//     temp.setTipo("k");
-//     var res = temp.kac();
-//     expect(res).to.equal(249726.85);
-//   });
-
-//   it("-21.3K === -498.01Fahrenheit", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(-21.3);
-//     temp.setTipo("K");
-//     var res = temp.kaf();
-//     expect(res).to.equal(-498.01);
-//   });
-
-//   it("32F === 0.0 Celsius", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(32);
-//     temp.setTipo("F");
-//     var res = temp.fac();
-//     expect(res).to.equal(0.0);
-//   });
-
-//   it("-18.3f !== 213.6 Kelvin", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(-18.3);
-//     temp.setTipo("f");
-//     var res = temp.fak();
-//     expect(res).not.to.equal(213.6);
-//   });
-
-//   it("Resultado = 65.1", function() {
-//     var temp = new Temperatura();
-//     temp.setValor(65.1);
-//     temp.setTipo("K");
-//     expect(temp.getValor()).to.equal(65.1);
-//     expect(temp.getTipo()).to.equal("K");
-//   });
-
-//   it("32.4c === 90.3 Farenheit, 305.5 Kelvin", function() {
-//      window.onload = function() {
-//        var temp = new Temperatura();
-//        var res = "90.3 Farenheit, 305.5 Kelvin";
-//        expect(aux.innerHTML).to.equal("90.3 Farenheit, 305.5 Kelvin");
-//      }
-//    });
-
-//   it("5X = ERROR", function() {
-//        window.onload = function() {
-//          var temp = new Temperatura(5,"X");
-//          calculate();
-//          expect(fin.innerHTML).to.match("/ERROR/");
-//       }
-//     });
-// });
