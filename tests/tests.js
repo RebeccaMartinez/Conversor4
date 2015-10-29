c9bd6129c9d4f846d284ad0911b34d0a692c413c
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-10-29 19:45:48 +0000

    añadidos tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,10 +2,10 @@ var expect = chai.expect;
 
 describe("Tests del Conversor", function() {
 
-    it("-3.4F === 3-19.67 Celsius, 253.48Kelvin", function() {
+    it("22F === 267.59Kelvin", function() {
       var temp = new Temperatura();
 
-      temp.setValor(-3.4);
+      temp.setValor(22);
       temp.setTipo("F");
 
       var espia = sinon.spy();
@@ -13,16 +13,34 @@ describe("Tests del Conversor", function() {
 
       expect(espia.called).to.be.true;
       expect(espia.calledOnce).to.be.true;
-      expect(espia.firstCall.calledWith(-3.4)).to.be.true;
+      expect(espia.firstCall.calledWith(22)).to.be.true;
       expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
 
-      var aux = temp.fak();
+      var aux = temp.fak().toFixed(2);
 
-      expect(aux).to.equal("253.48Kelvin");
+      expect(aux).to.equal('267.59');
     });
 
+    it("11.1F === -11.61 Celsius", function() {
+      var temp = new Temperatura();
 
-    it("41.4e3c === 3-19.67 Celsius, 253.48Kelvin", function() {
+      temp.setValor(11.1);
+      temp.setTipo("F");
+
+      var espia = sinon.spy();
+      espia(temp.getValor());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith(11.1)).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
+
+      var aux = temp.fac().toFixed(2);
+
+      expect(aux).to.equal('-11.61');
+    });
+
+    it("41.4e3c === 74552.00 Fahrenheit", function() {
       var temp = new Temperatura();
 
       temp.setValor(41.4e3);
@@ -36,13 +54,32 @@ describe("Tests del Conversor", function() {
       expect(espia.firstCall.calledWith(41.4e3)).to.be.true;
       expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
 
-      var aux = temp.calculate();
+      var aux = temp.caf().toFixed(2);
 
-      expect(aux).to.equal("74552.00 Fahrenheit, 41673.15Kelvin");
+      expect(aux).to.equal('74552.00');
+    });
+
+    it("-2e3C === -1726.85Kelvin", function() {
+      var temp = new Temperatura();
+
+      temp.setValor(-2e3);
+      temp.setTipo("C");
+
+      var espia = sinon.spy();
+      espia(temp.getValor());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith(-2e3)).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
+
+      var aux = temp.cak().toFixed(2);
+
+      expect(aux).to.equal('-1726.85');
     });
 
 
-    it("-6.3k === 3-19.67 Celsius, 253.48Kelvin", function() {
+    it("-6.3k === -279.45Celsius", function() {
       var temp = new Temperatura();
 
       temp.setValor(-6.3);
@@ -56,9 +93,28 @@ describe("Tests del Conversor", function() {
       expect(espia.firstCall.calledWith(-6.3)).to.be.true;
       expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
 
-      var aux = temp.calculate();
+      var aux = temp.kac().toFixed(2);
 
-      expect(aux).to.equal("-279.45Celsius, -471.01Fahrenheit");
+      expect(aux).to.equal('-279.45');
+    });
+
+    it("303.3K === 86.27Fahrenheit", function() {
+      var temp = new Temperatura();
+
+      temp.setValor(303.3);
+      temp.setTipo("k");
+
+      var espia = sinon.spy();
+      espia(temp.getValor());
+
+      expect(espia.called).to.be.true;
+      expect(espia.calledOnce).to.be.true;
+      expect(espia.firstCall.calledWith(303.3)).to.be.true;
+      expect(espia.firstCall.calledWith(sinon.match.number)).to.be.true;
+
+      var aux = temp.kaf().toFixed(2);
+
+      expect(aux).to.equal('86.27');
     });
 
     it("Resultado: 22e3K", function() {
@@ -99,4 +155,5 @@ describe("Tests del Conversor", function() {
 
 
 });
+
 
